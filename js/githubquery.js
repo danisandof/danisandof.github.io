06ee30b2270c49daf255aa3dc2b93fa357d8f349
Daniel <daniel@email.com>
2023-06-21 23:36:21 -0500

    modificacion de la UI

--- js/githubquery.js
+++ js/githubquery.js
@@ -1,12 +1,1 @@
-fetch("https://api.github.com/users/danisandof/repos").then(function (response) {
-    response.json().then(function (repos) {
-        repos.splice(2, 1);
-        for (repo of repos) {
-            const repoitem = document.createElement("li");
-            repoitem.innerHTML = "<div class=\"uk-card uk-card-default uk-card-body\"><h3 class=\"uk-card-title\">" + repo.name + "</h3><p>" + repo.description + "</p><a href=" + repo.html_url + ">Visitar</a></div>";
-            document.getElementById("repos").appendChild(repoitem);
-        }
-    });
-}).catch(function (err) {
-    console.log(err);
-});
+fetch("https://api.github.com/users/danisandof/repos").then(function(e){e.json().then(function(e){for(repo of(e.splice(2,1),e)){const e=document.createElement("li");e.innerHTML='<div class="uk-card uk-card-default uk-card-body"><div class="card-loader"><div class="loader"><div></div><div></div><div></div></div></div><div class="uk-card-badge uk-label">Github</div><h3 class="uk-card-title">'+repo.name+"</h3><p>"+repo.description+"</p><a href="+repo.html_url+">Visitar</a></div>",document.getElementById("repos").appendChild(e)}})}).catch(function(e){console.log(e)}),window.addEventListener("load",function(){const e=document.getElementById("left-loader"),d=document.getElementById("cover-loader");if(screen.width<=1024){e.remove(),d.remove();const o=document.querySelectorAll(".card-loader");o.forEach(e=>{e.remove()})}else setTimeout(()=>{e.remove(),d.remove();const o=document.querySelectorAll(".card-loader");o.forEach(e=>{e.remove()})},5e3)});
